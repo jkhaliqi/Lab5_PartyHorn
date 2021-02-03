f8f32fadd164e7846aad23e80499f4a4d11a309b
Jacob Khaliqi <jkhaliqi@ucsd.edu>
2021-02-02 21:30:29 -0800

    commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,10 +2,22 @@
 
 // TODO
 
+//document.getElementById("sound-image").src="./assets/media/images/car.svg";
 
-var sound = document.getElementById('party-horn-form');
+var form = document.getElementById("party-horn-form");
+form.addEventListener("click",changeImage);
+function changeImage() {
+    document.getElementById("sound-image").src="./assets/media/images/car.svg";
+    document.getElementById("sound-image").src="./assets/media/images/air-horn.svg";
+
+    //change the image accroding to the radio button it is on
+}
+
+
+var partySound = document.getElementById("party-horn-form");
 //sound. loook into events that can change this into playng the sound
-sound.addEventListener("submit",playSound);
+partySound.addEventListener("submit",playSound);
+
 function playSound(){
     var audio = document.getElementById("horn-sound");
     var volume = document.getElementById("volume-number");
