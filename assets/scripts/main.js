3fd5646f1b43d845cdfe11b54753b3d983deb8b7
Jacob Khaliqi <jkhaliqi@ucsd.edu>
2021-02-02 18:23:42 -0800

    sound

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,16 @@
 // main.js
-//boutta get to work and push it on it 
+
 // TODO
+
+
+var sound = document.getElementById('party-horn-form');
+//sound. loook into events that can change this into playng the sound
+sound.addEventListener("submit",playSound);
+function playSound(){
+    var audio = document.getElementById("horn-sound");
+    var volume = document.getElementById("volume-number");
+    audio.volume = 0.5; //extrct volume entered from user on the form
+    alert(audio.volume);
+    audio.play();
+    alert(audio.volume);
+}
