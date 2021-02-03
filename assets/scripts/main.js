053154c68baed5c8f27bae9ef0132ac93935426b
Jacob Khaliqi <jkhaliqi@ucsd.edu>
2021-02-03 12:43:29 -0800

    1, 2, 3 bullets

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,18 +7,47 @@
 
 
 
-//click
+//click changes volume
 let number = document.getElementById("volume-number");
 let slide = document.getElementById("volume-slider");
+let volumeImg = document.getElementById("volume-image");
+
 number.addEventListener("input", changeNumber);
 function changeNumber() {
     slide.value = number.value;
+    if(slide.value <= 100){
+        volumeImg.src="./assets/media/icons/volume-level-3.svg";
+    }
+    if(slide.value <= 66){
+        volumeImg.src="./assets/media/icons/volume-level-2.svg";
+    }
+    if(slide.value <= 33){
+        volumeImg.src="./assets/media/icons/volume-level-1.svg";
+    }
+    if(slide.value <= 0){
+        volumeImg.src="./assets/media/icons/volume-level-0.svg";
+    }
 }
-//slide
+
+//slide changes volume
 slide.addEventListener("input", slideNumber) 
 function slideNumber() {
     number.value = slide.value;
+    if(number.value <= 100) {
+        volumeImg.src="./assets/media/icons/volume-level-3.svg";
+    }
+    if(number.value <= 66) {
+        volumeImg.src="./assets/media/icons/volume-level-2.svg";
+    }
+    if(number.value <= 33) {
+        volumeImg.src="./assets/media/icons/volume-level-1.svg";
+    }
+    if(number.value <= 0) {
+        volumeImg.src="./assets/media/icons/volume-level-0.svg";
+    }
+
 }
+
 
 
 
@@ -40,7 +69,7 @@ partySound.addEventListener("submit",playSound);
 function playSound(){
     var audio = document.getElementById("horn-sound");
     var volume = document.getElementById("volume-number");
-    audio.volume = 0.5; //extrct volume entered from user on the form
+    audio.volume = 0.1; //extrct volume entered from user on the form
     alert(audio.volume);
     audio.play();
     alert(audio.volume);
